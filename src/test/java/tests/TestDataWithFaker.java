87ff9f6a2f64f7d946cf3a8da4e8f94c8025e36a
Sergei Volkov <Sneedjik@gmail.com>
2024-10-31 17:08:35 +0600

    moved test data to TestDataWithFaker

---
+++ src/test/java/tests/TestDataWithFaker.java
@@ -0,0 +1,41 @@
+package tests;
+
+import net.datafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestDataWithFaker {
+
+    public static Map<String, String[]> statesWithCities = new HashMap<>() {{
+        put("NCR", new String[]{"Delhi", "Gurgaon", "Noida"});
+        put("Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"});
+        put("Haryana", new String[]{"Karnal", "Panipat"});
+        put("Rajasthan", new String[]{"Jaipur", "Jaiselmer"});
+    }};
+    static Faker faker = new Faker();
+    public static int age = faker.number().numberBetween(0, 124);
+    public static LocalDate birthDate = LocalDate.now()
+            .minus(Period.ofYears(age));
+    static String[] genders = {"Male", "Female", "Other"};
+    static String[] subjects = {"Maths", "Computer Science", "Chemistry", "Biology", "History", "English"};
+    static String[] hobbies = {"Reading", "Sports", "Music"};
+    public static String dayOfBirth = String.format("%02d", birthDate.getDayOfMonth()),
+            monthOfBirth = birthDate.getMonth().name().substring(0, 1).toUpperCase() + birthDate.getMonth().name().substring(1).toLowerCase(),
+            yearOfBirth = String.valueOf(birthDate.getYear()),
+            firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            gender = faker.options().option(genders),
+            number = faker.number().digits(10),
+            badNumber = faker.regexify("[A-Za-z]{3}"),
+            subject = faker.options().option(subjects),
+            hobby = faker.options().option(hobbies),
+            picture = "One-Punch Man.jpg",
+            address = faker.address().streetAddress(),
+            state = faker.options().option(statesWithCities.keySet().toArray(new String[0])),
+            city = faker.options().option(statesWithCities.get(state));
+
+}
--- src/test/java/tests/UserRegistrationWithFakeDataTest.java
+++ src/test/java/tests/UserRegistrationWithFakeDataTest.java
@@ -1,49 +1,13 @@
 package tests;
 
-import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.HashMap;
-import java.util.Map;
+import static tests.TestDataWithFaker.*;
 
 public class UserRegistrationWithFakeDataTest extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
-
-    Faker faker = new Faker();
-
-    String[] genders = {"Male", "Female", "Other"};
-    String[] subjects = {"Maths", "Computer Science", "Chemistry", "Biology", "History", "English"};
-    String[] hobbies = {"Reading", "Sports", "Music"};
-
-    int age = faker.number().numberBetween(0, 124);
-    LocalDate birthDate = LocalDate.now()
-            .minus(Period.ofYears(age));
-    String dayOfBirth = String.format("%02d", birthDate.getDayOfMonth());
-    String monthOfBirth = birthDate.getMonth().name().substring(0, 1).toUpperCase() + birthDate.getMonth().name().substring(1).toLowerCase();
-    String yearOfBirth = String.valueOf(birthDate.getYear());
-    Map<String, String[]> statesWithCities = new HashMap<>() {{
-        put("NCR", new String[]{"Delhi", "Gurgaon", "Noida"});
-        put("Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"});
-        put("Haryana", new String[]{"Karnal", "Panipat"});
-        put("Rajasthan", new String[]{"Jaipur", "Jaiselmer"});
-    }};
-    String firstName = faker.name().firstName();
-    String lastName = faker.name().lastName();
-    String email = faker.internet().emailAddress();
-    String gender = faker.options().option(genders);
-    String number = faker.number().digits(10);
-    String badNumber = faker.regexify("[A-Za-z]{3}");
-    String subject = faker.options().option(subjects);
-    String hobby = faker.options().option(hobbies);
-    String picture = "One-Punch Man.jpg";
-    String address = faker.address().streetAddress();
-    String state = faker.options().option(statesWithCities.keySet().toArray(new String[0]));
-    String city = faker.options().option(statesWithCities.get(state));
-
 
     @Test
     void userRegistrationWithFakeDataTest() {
